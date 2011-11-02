781a965b37b88447329d4b1f56b00a6a39a5378b
Mathias <mathias@decodified.com>
2011-11-02 14:31:07 +0100

    Improve tests

--- src/test/scala/com/decodified/scalassh/SshClientSpec.scala
+++ src/test/scala/com/decodified/scalassh/SshClientSpec.scala
@@ -8,19 +8,33 @@ import org.specs2.execute.{Failure, FailureException}
 class SshClientSpec extends Specification { def is =
 
   "The SshClient should be able to" ^
-    "properly connect to the test host and fetch a directory listing" ! simpleTest
+    "properly connect to the test host and fetch a directory listing"           ! simpleTest^
+    "properly connect to the test host and execute three independent commands"  ! threeCommandsTest
 
   def simpleTest = {
     SSH(testHostName) { client =>
       client.exec("ls -a").right.map { result =>
-        val string = result.stdOutAsString() + "|" + result.stdErrAsString()
-        client.close()
-        string
+        result.stdOutAsString() + "|" + result.stdErrAsString()
       }
     }.right.get must startWith(".\n..\n")
   }
 
-  def testHostName = {
+  def threeCommandsTest = {
+    SSH(testHostName) { client =>
+      client.exec("ls").right.flatMap { res1 =>
+        println("OK 1")
+        client.exec("dfssgsdg").right.flatMap { res2 =>
+          println("OK 2")
+          client.exec("uname").right.map { res3 =>
+            println("OK 3")
+            (res1.exitCode, res2.exitCode, res3.exitCode)
+          }
+        }
+      }
+    }.right.get mustEqual (Some(0), Some(127), Some(0))
+  }
+
+  lazy val testHostName = {
     val fileName = HostFileConfig.DefaultHostFileDir + File.separator + ".testhost"
     try {
       Source.fromFile(fileName).getLines().toList.head
